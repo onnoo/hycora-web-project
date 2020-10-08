ed63d9fdb89262b2eaddce94acdb36803b261c86
MinjaeLee <sochubert@gmail.com>
2020-10-08 17:37:14 +0900

    update

--- app.js
+++ app.js
@@ -28,24 +28,15 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-//error handlers
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      err: err
-    });
-  });
-}
-
-//production error handler
+// error handler
 app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    errror: {}
-  });
+  res.render('error');
 });
 
 
