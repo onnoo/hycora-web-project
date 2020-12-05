5c93bddd7983a51e1ef355d5f0bde8e68f5c7fb4
onnoo <sweyjw@gmail.com>
2020-12-06 01:45:36 +0900

    complete final

--- routes/event.js
+++ routes/event.js
@@ -1,35 +1,38 @@
-var express = require('express');
-var moment = require('moment');
+const { response } = require("express");
+var express = require("express");
+const fetch = require("node-fetch");
 var router = express.Router();
 
-var mysql      = require('mysql');
+var mysql = require("mysql");
 var connection = mysql.createConnection({
-  host     : 'un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  user     : 'g9nik0gsfr2xcvl0',
-  password : 'fw5levjz4ldksb6c',
-  database : 'zp4vot7umqtxfib1'
+  host: "un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "g9nik0gsfr2xcvl0",
+  password: "fw5levjz4ldksb6c",
+  database: "zp4vot7umqtxfib1",
 });
 
 connection.connect();
 
-router.get('/', function(req, res, next) {
-  connection.query('SELECT * from events ORDER BY DATE DESC', function (error, results, fields) {
-    if (error) {
-      console.log(error);
-    }
-    // 쿼리 결과를 EJS로 넘겨준다.
-    res.render('event', { title: 'HY-CoRA', userData: results, moment: moment })
-  });
+router.get("/", function (req, res, next) {
+  fetch("http://hycora-temp.herokuapp.com/test")
+    .then((response) => response.json())
+    .then((data) => {
+      res.render("event", { title: "HY-CoRA", rank: data['rank'] });
+    });
 });
 
-router.post('/addComment',function (req,res,next){
+router.post("/addComment", function (req, res, next) {
   var content = req.body.insertform;
-  connection.query('insert into events(CONTENT) VALUE(?)',[content],function (error, results, fields) {
-    if (error) {
-      console.log(error);
+  connection.query(
+    "insert into events(CONTENT) VALUE(?)",
+    [content],
+    function (error, results, fields) {
+      if (error) {
+        console.log(error);
+      }
+      res.redirect("/event");
     }
-    res.redirect('/event');
-  })
-})
+  );
+});
 
 module.exports = router;
